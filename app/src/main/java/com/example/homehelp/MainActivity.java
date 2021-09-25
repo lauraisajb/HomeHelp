7087f2a51ad001e56431a2a4a635636ca7c8e1e0
Andersson <anderson.980114@gmail.com>
2021-09-25 16:54:31 -0500

    primer Commit

---
+++ app/src/main/java/com/example/homehelp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.homehelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //SINGUP
+    public void singUp(View view){
+        Intent singUp = new Intent(this, SingUp.class);
+        startActivity(singUp);
+    }
+}
