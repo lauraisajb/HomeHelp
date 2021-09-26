2cbbba954dfaa82fbb74aeadbc509faba06cbe9c
Andersson <anderson.980114@gmail.com>
2021-09-26 00:14:38 -0500

    maqueteado registro casi completo

--- app/src/main/java/com/example/homehelp/SingUp.java
+++ app/src/main/java/com/example/homehelp/SingUp.java
@@ -5,28 +5,93 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.Calendar;
 
 public class SingUp extends AppCompatActivity implements View.OnClickListener {
+    //FECHA
     ImageButton btnDate;
     EditText editTextDate;
     private  int dd, mm, yy;
+    //CIUDAD
+    Spinner comboCity;
+    //Usuario
+    Spinner comboUser;
+    //Oficio
+    Spinner comboOficio;
+    //descripcion
+    MultiAutoCompleteTextView descripcion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
 
+        //FECHA
         btnDate = (ImageButton)findViewById(R.id.BtnDate);
         editTextDate = (EditText)findViewById(R.id.editTextDate);
         btnDate.setOnClickListener(this);
 
+        //CIUDAD
+        comboCity = (Spinner) findViewById(R.id.spinnerCiudad);
+
+        ArrayAdapter<CharSequence> adapterCity = ArrayAdapter.createFromResource(this,
+                R.array.Ciudad, android.R.layout.simple_spinner_item);
+        comboCity.setAdapter(adapterCity);
+
+        //Usuario
+        comboUser = (Spinner) findViewById(R.id.spinnerUser);
+
+        ArrayAdapter<CharSequence> adapterUser = ArrayAdapter.createFromResource(this,
+                R.array.Usuario, android.R.layout.simple_spinner_item);
+        comboUser.setAdapter(adapterUser);
+
+
+        //Oficio
+        comboOficio = (Spinner) findViewById(R.id.spinnerOficio);
+
+        ArrayAdapter<CharSequence> adapterOficio = ArrayAdapter.createFromResource(this,
+                R.array.Oficio, android.R.layout.simple_spinner_item);
+        comboOficio.setAdapter(adapterOficio);
+
+        //descripcion
+        descripcion = (MultiAutoCompleteTextView)findViewById(R.id.editDescripcion);
+
+
+        //visibilidad
+        comboUser.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 2) {   //2 es operador
+                    comboOficio.setVisibility(View.VISIBLE);//visinle
+                    descripcion.setVisibility(View.VISIBLE);
+                } else {
+                    comboOficio.setVisibility(View.GONE);// INVISIBLE
+                    descripcion.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                //  tu código
+            }
+        });
+
+
+
     }
 
+    //FECHA
     @Override
     public void onClick(View v) {
         if (v== btnDate){
@@ -45,5 +110,7 @@ public class SingUp extends AppCompatActivity implements View.OnClickListener {
             ,dd, mm, yy);
             datePickerDialog.show();
         }
+
+
     }
 }
